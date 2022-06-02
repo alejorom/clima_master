b68f6d8e83660dc191b09651bcb62768028d3dd5
alejorom <morojela-github@yahoo.com>
2022-06-02 16:01:19 -0500

    Agregar llamado a http.dart libreria

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:clima_master/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -21,10 +22,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     getLocation();
   }
+
+  void getData() async
+  {
+    Uri url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      "lat": "35",
+      "lon": "139",
+      "appid": "3f174b0c82263e31ce9b4523b146ed43"
+    });
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    }
+    else {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    getData();
+    return Scaffold();
   }
 }
